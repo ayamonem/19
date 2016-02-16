66067bdbc5ffddac117035e0369944ecc8d51952
ayamonem <moon2020_moon2020@yahoo.com>
2016-02-16 19:44:45 +0200

    14-Absolute url reverse and namespace

--- src/test_project/urls.py
+++ src/test_project/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 # u can finad more regular expresions examples here https://github.com/codingforentrepreneurs/Guides/blob/master/all/common_url_regex.md
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^posts/',include("posts.urls")),
+    url(r'^posts/',include("posts.urls",namespace='posts')),
 ]
