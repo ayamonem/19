a8eabd0b1663e1d54fee352a8218f8992d180dcf
ayamonem <moon2020_moon2020@yahoo.com>
2016-02-16 17:10:19 +0200

    8-Create CRUD for posts (create,retreve,update,delete)

--- src/posts/urls.py
+++ src/posts/urls.py
@@ -1,6 +1,13 @@
 from django.conf.urls import url
 from django.contrib import admin
+from . import views
 # u can finad more regular expresions examples here https://github.com/codingforentrepreneurs/Guides/blob/master/all/common_url_regex.md
 urlpatterns = [
-    url(r'^$', "posts.views.post_home"),
+	url(r'^$', "posts.views.post_create"),
+    url(r'^create/$', "posts.views.post_create"),
+    url(r'^detail/$', "posts.views.post_detail"),
+    url(r'^list/$', "posts.views.post_list"),
+    url(r'^delete/$', "posts.views.post_delete"),
+    url(r'^update/$', "posts.views.post_update"),
+
 ]
--- src/posts/views.py
+++ src/posts/views.py
@@ -3,6 +3,14 @@ from django.shortcuts import render
 
 # Create your views here.
 
-
-def post_home(request):
-	return HttpResponse("<h1>Hello</h1>")
+# we will create CRUD(Create ,Retreve,Update,Delete)
+def post_create(request):
+	return HttpResponse("<h1>Create</h1>")
+def post_detail(request):#for retreve
+	return HttpResponse("<h1>Detail</h1>")
+def post_list(request):#for list items
+	return HttpResponse("<h1>List</h1>")
+def post_update(request):
+	return HttpResponse("<h1>Update</h1>")
+def post_delete(request):
+	return HttpResponse("<h1>Delete</h1>")
