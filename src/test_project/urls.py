2935e53efbb5f4822291d940258d48824cea2bcd
ayamonem <moon2020_moon2020@yahoo.com>
2016-02-16 16:29:57 +0200

    link

--- src/test_project/urls.py
+++ src/test_project/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-
+# u can finad more regular expresions examples here https://github.com/codingforentrepreneurs/Guides/blob/master/all/common_url_regex.md
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^posts/', "posts.views.post_home"),
