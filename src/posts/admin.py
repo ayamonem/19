81ac1252c046b30454fa28f95efdb5e1d423d282
ayamonem <moon2020_moon2020@yahoo.com>
2016-02-16 14:13:06 +0200

    4- Add Model Post to Admin

--- src/posts/admin.py
+++ src/posts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Post
+#we add the models to admin in this file
+# where Post is the name of class in models.py file
+
+admin.site.register(Post)
