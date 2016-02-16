048f288baed0b1e11b292636e08952c49984461d
ayamonem <moon2020_moon2020@yahoo.com>
2016-02-16 19:07:45 +0200

    13-Dynamic Url

--- src/posts/views.py
+++ src/posts/views.py
@@ -7,8 +7,8 @@ from .models import Post
 def post_create(request):
 	return HttpResponse("<h1>Create</h1>")
 
-def post_detail(request):#for retreve
-	instance=get_object_or_404(Post,id=1)
+def post_detail(request,id=None):#for retreve
+	instance=get_object_or_404(Post,id=id)
 	context={"title": instance.title,"instance": instance}
 	return render(request, "post_detail.html", context) #we are calling template --> {} from settings file 
 
