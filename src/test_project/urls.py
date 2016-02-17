4857b59b71a6b4874ae020d898d6211f360ccbbb
ayamonem <moon2020_moon2020@yahoo.com>
2016-02-17 19:51:58 +0200

    20-Setting Up Static Files
    https://docs.djangoproject.com/en/1.9/howto/static-files/
    
    1- // adding these lines to settings.py
    STATICFILES_DIRS = [
    os.path.join(BASE_DIR, "static"),
    #'/var/www/static/',
    ]
    
    STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn")
    MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "media_cdn")
    
    2- create static folder  in src folder,and static_cdn in virtual
    enviroment folder called 19
    3- run command:
    python manage.py collectstatic

--- src/test_project/urls.py
+++ src/test_project/urls.py
@@ -13,10 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.conf.urls import include,url
 from django.contrib import admin
+from django.conf.urls.static import static
 # u can finad more regular expresions examples here https://github.com/codingforentrepreneurs/Guides/blob/master/all/common_url_regex.md
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^posts/',include("posts.urls",namespace='posts')),
+    url(r'^posts/', include("posts.urls", namespace='posts')),
+    #url(r'^posts/$', "<appname>.views.<function_name>"),
 ]
+# u can see how to setup static files in https://docs.djangoproject.com/en/1.9/howto/static-files/
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
